4ccc28d9b9f9d5ddcabf6e626b4e0d680e69d0e3
Brandon <turk.brandon@gmail.com>
2014-09-23 09:59:26 -0700

    non-truncated filenames aren't given title texts

--- swfsauce.coffee
+++ swfsauce.coffee
@@ -1,6 +1,9 @@
 fileinfo = $ '.fileinfo'
 fileinfo.each ->
 	filename = $ '.postfilename', this
-	swfName = filename.attr("title")
+	if filename.attr "title" 
+		swfName = filename.attr "title"
+	else
+		swfName = filename.text()
 	if swfName.match /\.swf$/
 		$(this).append ' <a class="swfsauce" href="http://eye.swfchan.com/search/?q='+swfName+'" target="_blank">[Check Sauce on SWFCHAN]</a>'
--- swfsauce.js
+++ swfsauce.js
@@ -7,7 +7,11 @@
   fileinfo.each(function() {
     var filename, swfName;
     filename = $('.postfilename', this);
-    swfName = filename.attr("title");
+    if (filename.attr("title")) {
+      swfName = filename.attr("title");
+    } else {
+      swfName = filename.text();
+    }
     if (swfName.match(/\.swf$/)) {
       return $(this).append(' <a class="swfsauce" href="http://eye.swfchan.com/search/?q=' + swfName + '" target="_blank">[Check Sauce on SWFCHAN]</a>');
     }
