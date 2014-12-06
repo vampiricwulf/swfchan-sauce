e4f6450d31f5cf76839898fe5e0ac42c33692928
Brandon <turk.brandon@gmail.com>
2014-12-05 16:50:33 -0800

    Clarifications

--- coffee/swfsauce&download.coffee
+++ coffee/swfsauce&download.coffee
@@ -5,5 +5,6 @@ fileinfo.each ->
 	filename = $ '.unimportant > a', this
 	swfName = filename.attr "download"
 	if swfName.match /\.swf$/
+		###Move the line below to above the "if" statement if you want it to apply to all files, not just swfs.###
 		$(this).append ' <a class="swfdownload" download="'+swfName+'" href="'+orighref+'">[Download]</a>'
 		$(this).append ' <a class="swfsauce" href="http://eye.swfchan.com/search/?q='+swfName+'" target="_blank">[Check Sauce on SWFCHAN]</a>'
--- swfsauce&download.js
+++ swfsauce&download.js
@@ -11,6 +11,8 @@
     filename = $('.unimportant > a', this);
     swfName = filename.attr("download");
     if (swfName.match(/\.swf$/)) {
+
+      /*Move the line below to above the "if" statement if you want it to apply to all files, not just swfs. */
       $(this).append(' <a class="swfdownload" download="' + swfName + '" href="' + orighref + '">[Download]</a>');
       return $(this).append(' <a class="swfsauce" href="http://eye.swfchan.com/search/?q=' + swfName + '" target="_blank">[Check Sauce on SWFCHAN]</a>');
     }
